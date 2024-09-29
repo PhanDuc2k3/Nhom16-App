31ceb38febf69258002d3c2ed02e0d3580818efd
chungtringuyen <nguyentrichung17042003@gmail.com>
2024-09-30 03:40:05 +0700

    làm lại trang đăng nhập

--- lib/main.dart
+++ lib/main.dart
@@ -46,3 +46,149 @@ class Auth extends StatelessWidget {
     );
   }
 }
+
+// import 'package:flutter/material.dart';
+
+// void main() {
+//   runApp(MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Quản lý chi tiêu',
+//       theme: ThemeData(
+//         primarySwatch: Colors.blue,
+//       ),
+//       home: LoginScreen(),
+//     );
+//   }
+// }
+
+// class LoginScreen extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       backgroundColor: Colors.white,
+//       body: Padding(
+//         padding: const EdgeInsets.all(16.0),
+//         child: Center(
+//           child: SingleChildScrollView(
+//             child: Column(
+//               mainAxisAlignment: MainAxisAlignment.center,
+//               children: [
+//                 Text(
+//                   'Chào mừng trở lại!',
+//                   style: TextStyle(
+//                     fontSize: 24,
+//                     fontWeight: FontWeight.bold,
+//                   ),
+//                 ),
+//                 SizedBox(height: 8),
+//                 Text(
+//                   'hãy đăng nhập  !',
+//                   style: TextStyle(
+//                     color: Colors.grey,
+//                     fontSize: 16,
+//                   ),
+//                 ),
+//                 SizedBox(height: 32),
+//                 TextField(
+//                   decoration: InputDecoration(
+//                     labelText: 'Email',
+//                     border: OutlineInputBorder(),
+//                   ),
+//                 ),
+//                 SizedBox(height: 16),
+//                 TextField(
+//                   obscureText: true,
+//                   decoration: InputDecoration(
+//                     labelText: 'Mật Khẩu',
+//                     border: OutlineInputBorder(),
+//                     suffixIcon: Icon(Icons.visibility_off),
+//                   ),
+//                 ),
+//                 SizedBox(height: 16),
+//                 Align(
+//                   alignment: Alignment.centerRight,
+//                   child: TextButton(
+//                     onPressed: () {
+//                       // Handle forgot password
+//                     },
+//                     child: Text('Quên Mật Khẩu?'),
+//                   ),
+//                 ),
+//                 SizedBox(height: 16),
+//                 ElevatedButton(
+//                   onPressed: () {
+//                     // Handle login
+//                   },
+//                   child: Text('Đăng Nhập'),
+//                   style: ElevatedButton.styleFrom(
+//                     minimumSize: Size(double.infinity, 50),
+//                     foregroundColor: Colors.red,
+//                   ),
+//                 ),
+//                 SizedBox(height: 32),
+//                 Row(
+//                   children: [
+//                     Expanded(child: Divider()),
+//                     Padding(
+//                       padding: const EdgeInsets.symmetric(horizontal: 8.0),
+//                       // child: Text('Hoặc tiếp tục với'),
+//                     ),
+//                     Expanded(child: Divider()),
+//                   ],
+//                 ),
+//                 SizedBox(height: 16),
+//                 // Row(
+//                 //   mainAxisAlignment: MainAxisAlignment.center,
+//                 //   children: [
+//                 //     ElevatedButton.icon(
+//                 //       onPressed: () {
+//                 //         // Handle Google login
+//                 //       },
+//                 //       icon: Image.asset('assets/google_icon.png', height: 24),
+//                 //       label: Text('Google'),
+//                 //       style: ElevatedButton.styleFrom(
+//                 //         foregroundColor: Colors.white,
+//                 //         // foregroundColor: Colors.black,
+//                 //         minimumSize: Size(140, 50),
+//                 //       ),
+//                 //     ),
+//                 //     SizedBox(width: 16),
+//                 //     ElevatedButton.icon(
+//                 //       onPressed: () {
+//                 //         // Handle Facebook login
+//                 //       },
+//                 //       icon: Icon(Icons.facebook),
+//                 //       label: Text('Facebook'),
+//                 //       style: ElevatedButton.styleFrom(
+//                 //         backgroundColor: Colors.blue,
+//                 //         minimumSize: Size(140, 50),
+//                 //       ),
+//                 //     ),
+//                 //   ],
+//                 // ),
+//                 // SizedBox(height: 32),
+//                 Row(
+//                   mainAxisAlignment: MainAxisAlignment.center,
+//                   children: [
+//                     Text('Không có tài khoản?'),
+//                     TextButton(
+//                       onPressed: () {
+//                         // Handle sign up
+//                       },
+//                       child: Text('Đăng ký ngay'),
+//                     ),
+//                   ],
+//                 ),
+//               ],
+//             ),
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
